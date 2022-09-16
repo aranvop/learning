52da08763545e6625d918f83a08e6053a9985d11
aranvop <aran13640@outlook.com>
2022-09-16 14:17:59 +0800

    修改以下

--- python/1_3_5.py
+++ python/1_3_5.py
@@ -1,5 +1,8 @@
+#编写函数： 一年 365 天， 每周工作 5 天，休息 2 天，休息日水平下降
+#0.01，工作日要努力到什么程度一年后的水平才与每天努力 1%所取得的效果（即
+#37.78 倍）一样呢？
 
-def HowEffect(day,degree,n):
+def HowEffect(day,degree,n):#计算一年后的效果
     if day==366:
         return degree
     if (day%6==0|day%7==0):
@@ -7,7 +10,7 @@ def HowEffect(day,degree,n):
     else :
         return HowEffect(day+1,degree*(n+1),n)
 
-def funn(n):
+def funn(n):#计算努力程度
     while(abs(HowEffect(1,1.0,n)-37.78)>0.000000000001):
         if (HowEffect(1,1.0,n)>37.78 ):
             #print(n)
@@ -16,4 +19,5 @@ def funn(n):
             #print(n)
             n=n+pow(37.78,1/365)-pow(HowEffect(1,1.0,n),1/365)
     return n
-print(HowEffect(1,1,funn(0)))
+print(HowEffect(1,1,funn(0.01)))
+print("工作日要努力到",funn(0.01),)
