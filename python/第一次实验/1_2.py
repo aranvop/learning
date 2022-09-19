275ff092f695c6179e135babf2255386164ecd27
aranvop <aran13640@outlook.com>
2022-09-19 08:21:14 +0800

    test

--- python/第一次实验/1_2.py
+++ python/第一次实验/1_2.py
@@ -1,8 +1,15 @@
 #利用 if 判断来制作一个猜数字的小游戏
 import random
+def inputNum(str):
+    while True:
+        try:
+            num = int(input(str))
+            return num
+        except ValueError:
+            print("您输入的不是整数，请再次尝试输入！")
+#input输入的只能是字符串，所以需要转换为int类型还可能不是数字需要抛出异常
 y=random.randint(0,100)
-print("请猜这个数是多少")
-x = int (input())#input输入的只能是字符串，所以需要转换为int类型
+x=inputNum("请猜这个数是多少")
 count=10
 while (count!=0):
     if x<y:
@@ -13,4 +20,8 @@ while (count!=0):
         print("right")
         break
     count=count-1
-    x=int(input("请继续猜"))
+    if (count!=0):
+        print("还有",count,"次机会")
+        x=inputNum("请继续猜")
+    else :
+        print("次数用完了，正确答案是：",y)
