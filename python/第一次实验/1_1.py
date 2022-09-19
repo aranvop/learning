b84249d91e420773254e165d9c78ade6c6933634
aranvop <aran13640@outlook.com>
2022-09-19 08:21:07 +0800

    test

--- python/第一次实验/1_1.py
+++ python/第一次实验/1_1.py
@@ -1,10 +1,18 @@
 import random
 y=random.randint(0,100)
 print("请猜这个数是多少")
-x = int (input())#input输入的只能是字符串，所以需要转换为int类型
+while True:
+    try:
+        x = int(input("请输入一个数字: "))
+        break
+    except ValueError:
+        print("您输入的不是整数，请再次尝试输入！")
+#input输入的只能是字符串，所以需要转换为int类型还可能不是数字需要抛出异常
 if x<y:
     print("too small")
-if x>y:
+    print("true number is :" ,y)
+elif x>y:
     print("too large")
-if x==y:
+    print("true number is :" ,y)
+else :
     print("right")
